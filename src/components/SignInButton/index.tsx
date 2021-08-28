1cf6c5bfa4db2feb4f66fe2f050b01a4d59f686b
Werick Nalyson <wericknalyson@gmail.com>
2021-08-28 15:18:12 -0300

    fea: Authentication with NextAuth, github provider

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,27 +1,28 @@
 
 import styles from './styles.module.scss';
+import { signIn, signOut, useSession } from 'next-auth/client'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
-export function SignInButton(){
 
-  const session = true;
+export function SignInButton(){
+  const [session] = useSession()
 
   return session ? (
     <button
     type="button"
     className={styles.signInButton}
-    onClick={ () => {} }
+    onClick={ () => signOut() }
     >
-      <img src="https://sujeitoprogramador.com/steve.png" alt="Foto do usuario" />
-      Olá Matheus
+      <img src={session.user.image} />
+      Olá, {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
     type="button"
     className={styles.signInButton}
-    onClick={ () => {} }
+    onClick={ () => signIn('github') }
     >
       <FaGithub color="#FFB800"/>
       Entrar com github
