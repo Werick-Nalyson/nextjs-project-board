4f99dbb886ab84369294432c9367fce2b0f5d01c
Werick Nalyson <wericknalyson@gmail.com>
2021-08-31 22:24:05 -0300

    feat: list donaters

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
 
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { useState } from 'react';
+import firebase from '../config/firebaseConnection';
+
 import styles from '../styles/styles.module.scss';
 
+type Data = {
+  id: string;
+  donate: boolean;
+  lastDonate: Date;
+  image: string;
+}
 
-export default function Home() {
+interface HomeProps{
+  data: string;
+}
+
+export default function Home({ data }: HomeProps ) {
+  const [donaters, setDonaters] = useState<Data[]>(JSON.parse(data));
+
   return (
     <>
     <Head>
@@ -20,11 +36,39 @@ export default function Home() {
         </p>
       </section>
 
-      <div className={styles.donaters}>
+      {/* <div className={styles.donaters}>
         <img src="https://sujeitoprogramador.com/steve.png" alt="Usuario 1" />
+      </div> */}
+
+      {donaters.length !== 0 && <h3>Apoiadores:</h3>}
+
+      <div className={styles.donaters}>
+       {donaters.map( item => (
+          <img key={item.image} src={item.image} alt="Apoiador" />
+       ))}
       </div>
 
     </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const donaters = await firebase.firestore().collection('users').get();
+
+  const data = JSON.stringify(donaters.docs.map( u => {
+    return{
+      id: u.id,
+      ...u.data(),
+    }
+  }))
+
+  return{
+    props: {
+      data
+    },
+    revalidate: 60 * 60 * 1 //1 hour
+  }
+
 }
